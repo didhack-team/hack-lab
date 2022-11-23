32a1b6f1cfa38b50eca3153adf375e8211562164
DidhacK <didhack@didhack.io>
2022-11-23 03:49:24 +0100

    [Task] - Tailwind installed. App component simplified

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="my-12 mx-12">
+        <span className='font-bold text-blue-600'>Hello {'<hack-lab />'}!</span>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// @todo !! can be imported from other css?
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './styles/index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,4 +24,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// @todo check if this is cool
+// reportWebVitals(console.log);
